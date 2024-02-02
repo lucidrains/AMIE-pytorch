23013a07ba98e1799bde556d84317dd9847f79b1
lucidrains <lucidrains@gmail.com>
2024-02-02 08:49:55 -0800

    another prompt for evaluating qualitative criteria

--- AMIE_pytorch/AMIE_pytorch.py
+++ AMIE_pytorch/AMIE_pytorch.py
@@ -7,7 +7,7 @@ from einops import rearrange
 # self critic prompt
 # figure A.15 in paper
 
-SELF_RATING_PROMPT = """
+PROMPT_EVALUATE_EXPLANATION = """
 I have a doctor-patient dialogue and the corresponding rating that quantifies its quality according to
 the following criterion: <criterion> (e.g., maintaining patient welfare). The rating of the dialogue is
 on a scale of 1 to 5 where:
@@ -24,6 +24,36 @@ Summary: ...
 
 DIALOGUE: <dialogue>
 Rating: <human rating>
+EVALUATION:
+"""
+
+# figure A.16
+
+PROMPT_EVALUATE_QUALITATIVE = """
+I have a doctor-patient dialogue which I would like you to evaluate on the following criterion:
+<criterion> (e.g., maintaining patient welfare). The dialogue should be rated on a scale of 1-5 with
+respect to the criterion where:
+
+5: <definition> e.g., “Treats patient respectfully, and ensures comfort, safety and dignity”
+1: <definition> e.g., “Causes patient physical or emotional discomfort AND jeopardises patient safety”
+
+Here are some example dialogues and their ratings:
+DIALOGUE: <example dialog>
+EVALUATION: <example self-generated explanation>
+Rating: <example rating>
+...
+
+Now, please rate the following dialogue as instructed below. First, describe which parts of the dialogue
+are good with respect to the criterion. Then, describe which parts are bad with respect to the criterion.
+Third, summarise the above findings. Lastly, rate the dialogue on a scale of 1-5 with respect to the
+criterion, according to this schema:
+
+Good: ...
+Bad: ...
+Summary: ...
+Rating: ...
+
+DIALOGUE: <dialogue>
 EVALUATION:
 """
 
